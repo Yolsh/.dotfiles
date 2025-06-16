dfc4212c8239bd38640a4ea463ceaeb9724c32c9
Yolsh <146455868+Yolsh@users.noreply.github.com>
2025-06-16 20:19:48 +0100

    fix installs

--- scripts/user.sh
+++ scripts/user.sh
@@ -20,7 +20,7 @@ Installing AUR Softwares
 "
 source $HOME/YArch/dotfiles/setup.conf
 
-sed ~/YArch/pkg-files/${DESKTOP_ENV}.txt | while read line
+sed -n '/'$INSTALL_TYPE'/q;p' ~/YArch/pkg-files/${DESKTOP_ENV}.txt | while read line
 do
   echo "INSTALLING: ${line}"
   sudo pacman -S --noconfirm --needed ${line}
@@ -31,7 +31,7 @@ git clone "https://aur.archlinux.org/$AUR_HELPER.git"
 cd ~/$AUR_HELPER
 makepkg -si --noconfirm
 # install aur packages
-sed ~/YArch/pkg-files/aur-pkgs.txt | while read line
+sed -n '/'$INSTALL_TYPE'/q;p' ~/YArch/pkg-files/aur-pkgs.txt | while read line
 do
   echo "INSTALLING: ${line}"
   $AUR_HELPER -S --noconfirm --needed ${line}
